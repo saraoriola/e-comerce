a7e91bdbf31dc9f43f16a35f97e07a6b5e1925b4
saraoriola <saraoriola@outlook.com>
2023-07-10 22:42:57 +0200

    middleware check

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,44 +1,62 @@
+// Importing models and Sequelize
 const { User, Token, Sequelize } = require('../models');
+
+// Importing Sequelize operators for querying
 const { Op } = Sequelize;
+
+// Importing JWT library for token verification
 const jwt = require('jsonwebtoken');
-const  {jwt_secret}  = require('../config/config.json')['development']
 
-const authMiddleware = async(req, res, next) => {
-    try {
-        const token = req.headers.authorization;
-        const payload = jwt.verify(token, jwt_secret);
-        const user = await User.findByPk(payload.id);
-        console.log(user)
-        const tokenFound = await Token.findOne({
-            where: {
-                [Op.and]: [
-                    { UserId: user.id },
-                    { token: token }
-                ]
-            }
-        });
-        if (!tokenFound) {
-            return res.status(401).send({ message: 'You are not authorized' });
-        }
-        req.user = user;
-        next();
-    } catch (error) {
-        console.log(error)
-        res.status(500).send({ error, message: 'Error validating access token' })
+// Importing JWT secret from config
+const { jwt_secret } = require('../config/config.json')['development'];
+
+// Middleware for authentication
+const authMiddleware = async (req, res, next) => {
+  try {
+    // Extracting token from headers
+    const token = req.headers.authorization;
+
+    // Verifying the token
+    const payload = jwt.verify(token, jwt_secret);
+
+    // Finding user based on the payload ID
+    const user = await User.findByPk(payload.id);
+    console.log(user);
+
+    // Checking if the token exists for the user
+    const tokenFound = await Token.findOne({
+      where: {
+        [Op.and]: [
+          { UserId: user.id },
+          { token: token }
+        ]
+      }
+    });
+
+    // If token is not found, user is not authorized
+    if (!tokenFound) {
+      return res.status(401).send({ message: 'You are not authorized' });
     }
-}
 
-
-const isAdmin = async(req, res, next) => {
-    const admins = ['admin','superadmin'];
-    if (!admins.includes(req.userId.role)) {
-        return res.status(403).send({
-            message: 'User does not have permission to permform this action.'
-        });
-    }
+    // Assigning the user to the request object for further use
+    req.user = user;
     next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error, message: 'Error validating access token' });
+  }
 };
 
-//EXPORT
-module.exports = { authMiddleware, isAdmin };
+// Middleware for checking if the user is an admin
+const isAdmin = async (req, res, next) => {
+  const admins = ['admin', 'superadmin'];
+  if (!admins.includes(req.userId.role)) {
+    return res.status(403).send({
+      message: 'User does not have permission to perform this action.'
+    });
+  }
+  next();
+};
 
+// Exporting the middlewares
+module.exports = { authMiddleware, isAdmin };
